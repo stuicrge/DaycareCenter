5bca2741c203a0a2d9e5f1c21e44d5a0b4b58ace
jenny1004 <hello.git.wosl1006@naver.com>
2023-04-10 14:58:39 +0900

    Jaehoon commit

--- src/main/java/prj/DaycareCenter/controller/HomeController.java
+++ src/main/java/prj/DaycareCenter/controller/HomeController.java
@@ -1,15 +1,26 @@
 package prj.DaycareCenter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
 
-    @GetMapping("/")
-    public String home(){
+    /*
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
         return "home";
     }
+    */
+    @GetMapping ("/")
+    public String home(){
+        return "home";
+}
 
 }
